51548f5c7cfff44282ce6f060a7417927a588117
Chloe Chen <chloechen408@gmail.com>
2021-10-12 15:56:42 -0400

    6.11 done

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -19,36 +19,6 @@ struct WeatherLocation: Codable {
         self.longitude = longitude
     }
     
-    func getData() {
-        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&exclude=minutely&units=imperial&appid=\(APIkeys.openWeatherKey)"
-        
-        print("We are accessing the url \(urlString)")
-        
-        // create URL
-        guard let url = URL(string: urlString) else {
-            print("ERROR: Could not create a URL from \(urlString)")
-            return
-        }
-        
-        // create session
-        let session = URLSession.shared
-        
-        // get data with .dataTask method
-        let task = session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print("ERROR: \(error.localizedDescription)")
-            }
-            
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: [])
-                print("\(json)")
-            } catch {
-                print("JSON ERROR: \(error.localizedDescription)")
-            }
-        }
-        
-        task.resume()
-        
-    }
+    
 }
 
